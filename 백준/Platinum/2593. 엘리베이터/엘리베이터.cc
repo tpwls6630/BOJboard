ada24d97251a72c4ec35d134a9ee0d49937bc6da
EsOTi <tpwls6630@naver.com>
2023-03-20 00:47:33 +0900

    [Platinum V] Title: 엘리베이터, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Platinum/2593. 엘리베이터/엘리베이터.cc
+++ 백준/Platinum/2593. 엘리베이터/엘리베이터.cc
@@ -19,7 +19,7 @@
 using namespace std;
 //#######################################################
 
-int n, m,s,e;
+int n, m, s, e;
 vector<pair<int, int>> ele;
 
 int gcd(const int a, const int b) {
@@ -28,13 +28,13 @@ int gcd(const int a, const int b) {
 }
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n >> m;
 	ele = vector<pair<int, int>>(m);
 	vector<vector<int>> E(m); //엘리베이터간의 간선(같은 층이 있음)
 	vector<bool> s_ele(m), e_ele(m); //시작층이 있는 엘레베이터, 도착층이 있는 엘레베이터
-	
+
 
 	FOR(i, m) {
 		cin >> ele[i].first >> ele[i].second;
@@ -65,34 +65,12 @@ int main() {
 		if (s_ele[i]) {
 			//bfs
 
-			priority_queue<array<int, 2>> pq;
-			vector<array<int, 2>> path(m, { INT_MAX,-1 });
-			int lastele = -1, least = INT_MAX;
-			pq.push({ 0,i });
-			path[i][0] = 0;
 
-			while (!pq.empty()) {
-				auto [d, cur] = pq.top(); pq.pop();
-				if (-d > path[cur][0]) continue;
-				if (e_ele[cur]) {
-					if (least >= -d) {
-						least = -d;
-						lastele = cur;
-					}
-				}
-				for (auto el : E[cur]) {
-					if (path[el][0] > -d + 1) {
-						path[el] = { -d + 1, cur };
-						pq.push({ -path[el][0],el });
-					}
-				}
-			}
-			/*
-			queue<array<int,3>> bfs;
-			vector<pair<bool,int>> visited(m, {false, -1});
+			queue<array<int, 3>> bfs;
+			vector<pair<bool, int>> visited(m, { false, -1 });
 			int a = INT_MAX;
 			int lastele = -1;
-			bfs.push({ i, 0 , -1});
+			bfs.push({ i, 0 , -1 });
 			visited[i].first = true;
 
 			while (!bfs.empty()) {
@@ -110,18 +88,18 @@ int main() {
 						visited[next] = { true, cur };
 					}
 				}
-			}*/
+			}
 			if (lastele == -1) continue;
-			if (asize >= least) {
-				asize = least;
+			if (asize > a) {
+				asize = a;
 				vector<int>().swap(ans);
 				ans.push_back(lastele);
 				while (!s_ele[lastele]) {
-					lastele = path[lastele][1];
+					lastele = visited[lastele].second;
 					ans.push_back(lastele);
 				}
 			}
-			
+
 		}
 	}
 
