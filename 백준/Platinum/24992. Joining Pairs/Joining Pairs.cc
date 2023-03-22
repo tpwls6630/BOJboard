32dae0653df2173eea562d98ccc773c12e915ebd
EsOTi <tpwls6630@naver.com>
2023-03-22 22:17:04 +0900

    [Platinum III] Title: Joining Pairs, Time: 108 ms, Memory: 11924 KB -BaekjoonHub

--- 백준/Platinum/24992. Joining Pairs/Joining Pairs.cc
+++ 백준/Platinum/24992. Joining Pairs/Joining Pairs.cc
@@ -23,20 +23,20 @@ using namespace std;
 
 long long w, h;
 int n;
-long long border(long long x, long long y) {
+int border(int x, int y) {
 	if (y == 0) {
-		return x;
+		return x - w - h;
 	}
 	else if (y == h) {
-		return 2 * w + h - x;
+		return w - x;
 	}
 	else if (x == w) {
-		return w + y;
+		return y - h;
 	}
 	else if (x == 0) {
-		return 2 * w + 2 * h - y;
+		return w + h - y;
 	}
-	return -1;
+	return INT_MAX;
 }
 
 int main() {
@@ -45,12 +45,12 @@ int main() {
 
 	cin >> w >> h >> n;
 
-	map<long long, int> bor;
+	map<int, int> bor;
 	FOR(i, n) {
 		int x1, y1, x2, y2;
 		cin >> x1 >> y1 >> x2 >> y2;
 		int b1 = border(x1, y1), b2 = border(x2, y2);
-		if (b1 == -1 || b2 == -1) continue;
+		if (b1 == INT_MAX || b2 == INT_MAX) continue;
 		bor.insert({ b1,i });
 		bor.insert({ b2,i });
 	}
