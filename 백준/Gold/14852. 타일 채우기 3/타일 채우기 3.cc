639816de3e589dec8d6475aef4c008f0c37b3661
EsOTi <tpwls6630@naver.com>
2023-03-24 17:27:30 +0900

    [Gold IV] Title: 타일 채우기 3, Time: 12 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/14852. 타일 채우기 3/타일 채우기 3.cc
+++ 백준/Gold/14852. 타일 채우기 3/타일 채우기 3.cc
@@ -3,11 +3,11 @@
 using namespace std;
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	long long sum = 0, n; cin >> n;
-	vector<long long> dp(n + 1);
+	int sum = 0, n; cin >> n;
+	vector<int> dp(n + 1);
 	dp[1] = 2; dp[2] = 7; sum = dp[1] + dp[2];
 	for (int i = 3; i <= n; i++) {
-		dp[i] = ((sum + 1) * 2 + dp[i - 2]) % 1'000'000'007;
+		dp[i] = (((sum + 1) * 2) % 1'000'000'007 + dp[i - 2]) % 1'000'000'007;
 		sum = (sum + dp[i]) % 1'000'000'007;
 	}
 	cout << dp[n];
